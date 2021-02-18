10385244d0e8397f61c9ae84e168b6f8ba16188b
Fidel Collin <fidelcollin@icloud.com>
2021-02-18 21:11:01 +0000

    started adding content to contact component and styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
 
 const Contact = () => {
-  return (
-    <div>
-      <h2>Contact Page</h2>
-    </div>
-  )
-}
 
-export default Contact
+const [input, setInput] = useState('')
+
+  return (
+    <section className="contact">
+      <h2>Contact Us</h2>
+      <h3>
+        Please feel free to get in touch with us here. We'll set you up with a
+        personal account.
+      </h3>
+      <p>Just add a message in the area below to request for an account.</p>
+      <article className="section">
+        <form className="contact-form">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" className="form-control" />
+          </div>
+        </form>
+      </article>
+    </section>
+  );
+};
+
+export default Contact;
